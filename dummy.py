a6d3bec92aeab4ddb45cf815db3c0cd15bc8c012
sam <harrysam024@gmail.com>
2025-07-10 02:56:51 +0530

    adding logger and exception

---
+++ dummy.py
@@ -0,0 +1,26 @@
+# main.py
+
+# import logging
+# from src import logger  # Triggers configure_logger() from __init__.py
+
+# Get a module-level logger
+# log = logging.getLogger(__name__)
+
+# log.debug("This is a debug message.")
+# log.info("This is an info message.")
+# log.warning("This is a warning message.")
+# log.error("This is an error message.")
+# log.critical("This is a critical message.")
+
+
+# # below code is to check the exception config
+import logging
+from src import logger
+from src import exception
+import sys
+log = logging.getLogger(__name__)
+try:
+    a = 1+'Z'
+except Exception as e:
+    log.info(e)
+    raise exception.CustomException(e, sys) from e
